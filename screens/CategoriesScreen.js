b16de0a4667312a8364ce8479fb086cbd3ae037b
Abhiroopmokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-11-28 11:22:22 +0530

    Added native-stack navigator

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+// console.log(CATEGORIES[0].id);
+
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    function pressHandler() {
+      navigation.navigate("MealsOverview");
+    }
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
